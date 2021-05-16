670f243771a6ca7035e9285d97ade834f3040edb
Angus Hollands <goosey15@gmail.com>
2021-05-16 13:02:07 +0100

    Feature: use traitlets for file-bsed configuration

--- vpn.py
+++ vpn.py
@@ -1,15 +1,15 @@
-import getpass
+import pathlib
 import re
-import shutil
-import zipfile
-import argparse
-from pathlib import Path
-from subprocess import run
+import subprocess
 from typing import Sequence
-from urllib.request import urlopen, urlretrieve
+from urllib.request import urlopen
 
+import traitlets.config
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
 VERSION_NUMBER_PATTERN = r"(\d+)\.(\d+)\.(\d+)\.(\d+)"
 
@@ -19,61 +19,104 @@ class VersionMismatch(Exception):
 
 
 def get_chrome_version() -> Sequence[str]:
-    result = run(['google-chrome', '--version'], capture_output=True, check=True).stdout.decode().strip()
+    result = (
+        subprocess.run(["google-chrome", "--version"], capture_output=True, check=True)
+        .stdout.decode()
+        .strip()
+    )
     return re.search(VERSION_NUMBER_PATTERN, result).groups()
 
 
 def get_chromedriver_version() -> Sequence[str]:
-    result = run(['chromedriver', '--version'], capture_output=True, check=True).stdout.decode().strip()
+    result = (
+        subprocess.run(["chromedriver", "--version"], capture_output=True, check=True)
+        .stdout.decode()
+        .strip()
+    )
     return re.search(VERSION_NUMBER_PATTERN, result).groups()
 
 
 def get_required_chromedriver_version(chrome_version: Sequence[str]) -> Sequence[str]:
-    version_string = '.'.join(chrome_version[:3])
-    with urlopen(f"https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{version_string}") as response:
+    version_string = ".".join(chrome_version[:3])
+    with urlopen(
+        f"https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{version_string}"
+    ) as response:
         return re.search(VERSION_NUMBER_PATTERN, response.read().decode()).groups()
 
 
-def install_chromedriver(chromedriver_version: Sequence[str]):
-    version_string = '.'.join(chromedriver_version)
-    zip_path, headers = urlretrieve(
-        f"https://chromedriver.storage.googleapis.com/{version_string}/chromedriver_linux64.zip"
+_VERSION_MISMATCH_ERROR = """
+Version mismatch between chromedriver version {version} and Chrome required version {required_version}.
+Download the correct chromedriver from https://chromedriver.storage.googleapis.com/{required_version_string}/chromedriver_linux64.zip
+"""
+
+
+class VPNApp(traitlets.config.Application):
+    name = "vpn"
+    description = "UoB VPN launcher"
+
+    aliases = {
+        "username": "VPNApp.username",
+        "password": "VPNApp.password",
+        "2fa": "VPNApp.two_factor",
+        "config": "VPNApp.config_file_path",
+    }
+    flags = {"full": ({"VPNApp": {"use_full_vpn": True}}, "use full VPN")}
+
+    config_file_path = traitlets.Unicode(
+        config=True,
+        help="Full path of a config file.",
+    )
+    username = traitlets.Unicode(config=True, help="username")
+    password = traitlets.Unicode(config=True, help="password")
+    two_factor = traitlets.Unicode(config=True, help="2fa code")
+    use_full_vpn = traitlets.Bool(
+        config=True,
+        help="Use the full VPN (for all connections).",
+    )
+    wait_duration = traitlets.Float(
+        config=True, help="delay before page load is considered a failure"
     )
 
-    # Extract zip
-    install_path = Path.home() / ".local" / "bin" / "chromedriver"
-    with zipfile.ZipFile(zip_path) as zip:
-        zip.extract("chromedriver", install_path.parent)
+    def _load_config_file(self):
+        if (path := pathlib.Path(self.config_file_path)).exists() and path.is_file():
+            self.load_config_file(path.name, str(path.parent))
 
-    # Check we can find this installed chromedriver
-    if Path(shutil.which("chromedriver")) != install_path:
-        raise RuntimeError(
-            f"newly installed chromedriver at {install_path} is not on system path ({shutil.which('chromedriver')})")
+        else:
+            self.load_config_file(
+                f"{self.name}_config",
+                [str(pathlib.Path.cwd()), str(pathlib.Path.home() / ".config")],
+            )
 
+    @traitlets.config.catch_config_error
+    def initialize(self, argv=None):
+        super().initialize(argv)
+        self._load_config_file()
 
-def main(argv=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--full-vpn", action="store_true")
-    args = parser.parse_args(argv)
+    def start(self):
+        version = get_chromedriver_version()
+        required_version = get_required_chromedriver_version(get_chrome_version())
 
-    username = input("username: ")
-    password = getpass.getpass("password: ")
-    two_factor = input("2fa: ")
+        if version != required_version:
+            raise VersionMismatch(
+                _VERSION_MISMATCH_ERROR.format(
+                    version=version,
+                    required_version=required_version,
+                    required_version_string=".".join(required_version),
+                )
+            )
 
-    chrome_version = get_chrome_version()
-    try:
-        if get_chromedriver_version() != get_required_chromedriver_version(chrome_version):
-            raise VersionMismatch("Version mismatch for chromedriver and chrome")
+        # Input validation
+        if not (username := self.username):
+            raise ValueError("Please specify non-empty username string")
+        if not (password := self.password):
+            raise ValueError("Please specify non-empty password string")
+        if not (two_factor := self.two_factor):
+            raise ValueError("Please specify non-empty two factor string")
 
-    except (FileNotFoundError, VersionMismatch):
-        required_chromedriver_version = get_required_chromedriver_version(chrome_version)
-        install_chromedriver(required_chromedriver_version)
-
-    # Create chromedriver
-    driver = webdriver.Chrome()
-
-    while True:
+        # Create chromedriver
+        driver = webdriver.Chrome()
         driver.get("https://remoteaccess.bham.ac.uk")
+
         while True:
             try:
                 driver.find_element_by_link_text("Click here to continue").click()
@@ -84,39 +127,31 @@ def main(argv=None):
         driver.find_element_by_id("input_2").send_keys(password)
         driver.find_element_by_id("input_3").send_keys(two_factor)
 
-        if args.full_vpn:
+        if self.use_full_vpn:
             driver.find_element_by_id("input_4").click()
 
         driver.find_element_by_xpath('//input[@type="submit"]').click()
 
         if driver.page_source.find("username or password is not correct") != -1:
-            print("Username, password, or 2fa was incorrect")
+            raise ValueError("Username, password, or 2fa was incorrect")
 
-            username = input("username: ") or username
-            password = getpass.getpass("password: ") or password
-            two_factor = input("2fa: ") or two_factor
+        if driver.page_source.find("session could not be established") != -1:
+            raise RuntimeError("An error occurred, re-try with new 2fa?")
 
-        elif driver.page_source.find("session could not be established") != -1:
-            print("An error occurred, re-try with new 2fa?")
+        # Launch VPN
+        required_id = (
+            "/Common/UoB_Research_full_NA"
+            if self.use_full_vpn
+            else "/Common/UoB_Research_NA"
+        )
+        launch_elem = WebDriverWait(driver, self.wait_duration).until(
+            expected_conditions.presence_of_element_located((By.ID, required_id))
+        )
+        launch_elem.click()
+        input()
 
-            two_factor = input("2fa: ")
-            if not two_factor:
-                return 1
-
-        else:
-            assert driver.title == "F5 Dynamic Webtop"
-            break
-
-    # Launch VPN
-    try:
-        elem = driver.find_element_by_id("/Common/UoB_Research_NA")
-    except NoSuchElementException:
-        elem = driver.find_element_by_id("/Common/UoB_Research_full_NA")
-    elem.click()
-    input()
-
-    return 0
+        return 0
 
 
 if __name__ == "__main__":
-    main()
+    VPNApp.launch_instance()
